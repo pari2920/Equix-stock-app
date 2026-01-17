735cc94b41e2b589ac0d2a17f376397aec9bec73
Pari Sethi <your-email@gmail.com>
2026-01-17 22:03:03 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <Button>Click me</Button>
+        </div>
+
+    )
+}
+export default Page
+
+
